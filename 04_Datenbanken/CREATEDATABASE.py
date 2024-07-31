a99c5848f51bc5afd2aa108b16678696cce8e89a
yannick Weber <yannick.weber@scaltel.de>
2024-07-31 14:18:00 +0200

    Bearbeitung Aufgabe 04

--- 04_Datenbanken/CREATEDATABASE.py
+++ 04_Datenbanken/CREATEDATABASE.py
@@ -5,6 +5,7 @@ import json
 
 db = mysql.connector.connect(host="localhost", user="root", password="root")
 cursor = db.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS superhero_db")
 
 
 class CreateDb:
@@ -31,6 +32,7 @@ class CreateDb:
 
         create_members_table = """
         CREATE TABLE IF NOT EXISTS members (
+            memberID INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(50),
             age INT,
             secretIdentity VARCHAR(50)
@@ -40,22 +42,71 @@ class CreateDb:
 
         create_powers_table = """
             CREATE TABLE IF NOT EXISTS powers (
-            power VARCHAR(50)
+            powerID INT AUTO_INCREMENT PRIMARY KEY,
+            powers VARCHAR(255)
             )
             """
         self.cursor.execute(create_powers_table)
 
         self.connection.commit()
 
+    def insertData(self):
+        with open("/home/yw/einarbeitung/03_Dateiformate/base1.json", "r") as file:
+            data = json.load(file)
+
+        for squad in data:
+            squadSql = """
+            INSERT INTO squads (squadName, homeTown, formed, status, secretBase, active)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """
+
+            squadValues = (
+                squad["squadName"],
+                squad["homeTown"],
+                squad["formed"],
+                squad["status"],
+                squad["secretBase"],
+                squad["active"],
+            )
+
+            self.cursor.execute(squadSql, squadValues)
+
+        for squad in data:
+            for member in squad["members"]:
+                memberSQL = """
+                INSERT INTO members (name, age, secretIdentity)
+                VALUES (%s, %s, %s)
+                """
+
+                memberValues = (
+                    member["name"],
+                    member["age"],
+                    member["secretIdentity"],
+                )
+
+                self.cursor.execute(memberSQL, memberValues)
+
+        for squad in data:
+            for member in squad["members"]:
+                for power in member["powers"]:
+                    powerSQL = """
+                        INSERT INTO powers (powers)
+                        VALUES (%s)
+                        """
+
+                    powerValue = (power,)
+
+                    self.cursor.execute(powerSQL, powerValue)
+        self.connection.commit()
+
 
 conn = mysql.connector.connect(
     host="localhost", user="root", password="root", database="superhero_db"
 )
-
-
 cursor = conn.cursor()
 cursor.execute("USE superhero_db")
 
 database = CreateDb(conn)
 database.createTables()
+database.insertData()
 conn.close()
